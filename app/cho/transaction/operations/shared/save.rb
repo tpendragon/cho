7ae0290cbfc236a499a7abbeb12cbe5a50cb4bf3
Carolyn Cole <cam156@psu.edu>
2018-07-05 14:35:06 -0400

    Adding transactions for creating, validating, and creating change sets for any object
    Modified change_set_persister to utilize the new transactions

---
+++ app/cho/transaction/operations/shared/save.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Transaction
+  module Operations
+    module Shared
+      class Save
+        attr_reader :metadata_adapter
+
+        include Dry::Transaction::Operation
+
+        def call(change_set, persister:)
+          change_set.sync
+          Success(persister.save(resource: change_set))
+        rescue StandardError => error
+          change_set.errors.add(:save, error.message)
+          Failure(change_set)
+        end
+      end
+    end
+  end
+end
